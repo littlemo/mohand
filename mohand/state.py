efa8b90de2e7d152ad69557898a62d25fe1573fc
Moore.Huang <moore@moorehy.com>
2018-04-30 12:43:07 +0800

    F: 实现全局环境字典，并添加'mohandfile'，'version'两个配置项

--- mohand/state.py
+++ mohand/state.py
@@ -3,3 +3,17 @@
 
 用以提供全局参数的声明
 """
+from mohand.utils import _AttributeDict
+from mohand.version import get_cli_version
+
+
+"""
+全局环境字典，包含所有的配置
+部分终端默认值会在终端命令定义时进行再初始化赋值
+"""
+env = _AttributeDict({
+    'version': get_cli_version(),
+
+    # 终端再赋值
+    'mohandfile': 'mohandfile.py',
+})
