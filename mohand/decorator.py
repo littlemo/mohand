b67a5d62999063783a4239bea0d738bf0c5e261d
Moore.Huang <moore@moorehy.com>
2018-05-01 19:50:51 +0800

    D: 准备改为使用stevedore插件加载来注册hand，故此处删除原先的注册装饰器

--- mohand/decorator.py
+++ mohand/decorator.py
@@ -3,17 +3,3 @@
 
 用以提供作为扩展插件开发所需的装饰器
 """
-from mohand.utils import _AttributeDict
-from mohand.exception import HandDuplicationOfNameError
-
-
-hand = _AttributeDict({
-    '_click': click,
-})
-
-
-def register(func):
-    if hasattr(hand, func.__name__):
-        raise HandDuplicationOfNameError(func.__name__)
-    hand[func.__name__] = func
-    return func
