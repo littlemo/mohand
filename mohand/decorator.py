3c11e4dcd7b55458f91023decc66473cf398c6d7
Moore.Huang <moore@moorehy.com>
2018-05-01 12:12:36 +0800

    O: 对注册装饰器逻辑进行hand重名判断，若重名则抛出相应的异常

--- mohand/decorator.py
+++ mohand/decorator.py
@@ -4,6 +4,7 @@
 用以提供作为扩展插件开发所需的装饰器
 """
 from mohand.utils import _AttributeDict
+from mohand.exception import HandDuplicationOfNameError
 
 
 hand = _AttributeDict({
@@ -12,5 +13,7 @@ hand = _AttributeDict({
 
 
 def register(func):
+    if hasattr(hand, func.__name__):
+        raise HandDuplicationOfNameError(func.__name__)
     hand[func.__name__] = func
     return func
