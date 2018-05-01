3370a7e79eb2195a7acdf749d06a171b5240156c
Moore.Huang <moore@moorehy.com>
2018-05-01 12:11:44 +0800

    F: 实现自定义的hand注册重名错误异常

---
+++ mohand/exception.py
@@ -0,0 +1,13 @@
+"""
+自定义异常类
+"""
+
+
+class HandDuplicationOfNameError(Exception):
+    """
+    Hand注册重名错误
+    """
+    def __init__(self, name, msg=None, *args, **kwargs):
+        msg = msg or '待注册的hand名已存在: {name}'
+        err = msg.format(name=name)
+        super().__init__(err, *args, **kwargs)
