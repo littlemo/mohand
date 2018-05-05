2a00327eac633575dec982245fbf620ef22b9c34
Moore.Huang <moore@moorehy.com>
2018-05-06 01:00:54 +0800

    F: 实现封装可调用方法的Hand子类，便于在装饰器中使用，封装目标方法

--- mohand/hands.py
+++ mohand/hands.py
@@ -76,4 +76,34 @@ class Hand(object):
         raise NotImplementedError
 
 
+class WrappedCallableHand(Hand):
+    """
+    透明封装一个传入的 ``callable_`` ，并将其标记为一个有效的 hand 。
+
+    通常通过一个扩展插件实现的相应功能的装饰器来使用，而非直接使用
+    """
+    def __init__(self, callable_, *args, **kwargs):
+        super(WrappedCallableHand, self).__init__(*args, **kwargs)
+        self.wrapped = callable_
+
+        if hasattr(callable_, '__name__'):
+            if self.name == 'undefined':
+                self.__name__ = self.name = callable_.__name__
+            else:
+                self.__name__ = self.name
+        if hasattr(callable_, '__doc__'):
+            self.__doc__ = callable_.__doc__
+        if hasattr(callable_, '__module__'):
+            self.__module__ = callable_.__module__
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        return self.wrapped(*args, **kwargs)
+
+    def __getattr__(self, k):
+        return getattr(self.wrapped, k)
+
+
 hand = HandDict()
