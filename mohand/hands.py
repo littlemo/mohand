76e04ef3831344e535a46bcc2c7c4691bdcd1342
Moore.Huang <moore@moorehy.com>
2018-05-06 20:19:29 +0800

    D: 重新思考实现方式，故删除Hand/WrappedCallableHand两个类的实现

--- mohand/hands.py
+++ mohand/hands.py
@@ -61,49 +61,4 @@ def load_hands():
     return handdict
 
 
-class Hand(object):
-    """
-    希望被提取为 MoHand 的 hand 的抽象基类
-
-    其子类的实例将被作为合法的 hand 从 handfile 中被 ``mohand`` 工具加载
-
-    对于如何使用 `~mohand.hands.Hand` 子类，可查看相关文档
-    """
-    def __init__(self, name=None, *args, **kwargs):
-        self.name = name or 'undefined'
-
-    def run(self):
-        raise NotImplementedError
-
-
-class WrappedCallableHand(Hand):
-    """
-    透明封装一个传入的 ``callable_`` ，并将其标记为一个有效的 hand 。
-
-    通常通过一个扩展插件实现的相应功能的装饰器来使用，而非直接使用
-    """
-    def __init__(self, callable_, *args, **kwargs):
-        super(WrappedCallableHand, self).__init__(*args, **kwargs)
-        self.wrapped = callable_
-
-        if hasattr(callable_, '__name__'):
-            if self.name == 'undefined':
-                self.__name__ = self.name = callable_.__name__
-            else:
-                self.__name__ = self.name
-        if hasattr(callable_, '__doc__'):
-            self.__doc__ = callable_.__doc__
-        if hasattr(callable_, '__module__'):
-            self.__module__ = callable_.__module__
-
-    def __call__(self, *args, **kwargs):
-        return self.run(*args, **kwargs)
-
-    def run(self, *args, **kwargs):
-        return self.wrapped(*args, **kwargs)
-
-    def __getattr__(self, k):
-        return getattr(self.wrapped, k)
-
-
 hand = HandDict()
