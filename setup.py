08b9cb3118aa50efb0d6486b098269cd25051567
Moore.Huang <moore@moorehy.com>
2018-05-13 21:05:42 +0800

    M: 修改开发状态为Beta

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Environment :: MacOS X',
         'Intended Audience :: Developers',
