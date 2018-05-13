a5e994f78debb498ce26a0962fd1dac359432712
Moore.Huang <moore@moorehy.com>
2018-05-13 21:55:49 +0800

    O: 优化Hand注册重名错误类中的编码格式

--- source/mohand/exception.py
+++ source/mohand/exception.py
@@ -7,6 +7,7 @@ class HandDuplicationOfNameError(Exception):
     """
     Hand注册重名错误
     """
+
     def __init__(self, name, msg=None, *args, **kwargs):
         msg = msg or '待注册的hand名已存在: {name}'
         err = msg.format(name=name)
