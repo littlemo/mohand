cdbb98301701b521539194c78a4a499f1f1db0d2
Moore.Huang <moore@moorehy.com>
2018-05-13 22:24:09 +0800

    O: 优化声明模块中的文档描述

--- source/mohand/state.py
+++ source/mohand/state.py
@@ -1,6 +1,4 @@
 """
-声明模块
-
 用以提供全局参数的声明
 """
 from collections import OrderedDict
@@ -9,10 +7,6 @@ from mohand.utils import _AttributeDict
 from mohand.version import get_cli_version
 
 
-"""
-全局环境字典，包含所有的配置
-部分终端默认值会在终端命令定义时进行再初始化赋值
-"""
 env = _AttributeDict({
     'version': OrderedDict({
         'mohand': get_cli_version(),
@@ -22,3 +16,7 @@ env = _AttributeDict({
     # 终端再赋值
     'handfile': 'handfile.py',
 })
+"""
+全局环境字典，包含所有的配置
+部分配置，如 ``version`` ，会在加载完扩展插件后进行再次填充
+"""
