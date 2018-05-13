8183b58c80ddd4e9b32f8ea8b5975bc8d2ebfecc
Moore.Huang <moore@moorehy.com>
2018-05-13 10:26:40 +0800

    F: 重构env中version的定义，使用有序字典，用于存储当前包与所有插件扩展包的版本信息

--- mohand/state.py
+++ mohand/state.py
@@ -3,6 +3,8 @@
 
 用以提供全局参数的声明
 """
+from collections import OrderedDict
+
 from mohand.utils import _AttributeDict
 from mohand.version import get_cli_version
 
@@ -12,7 +14,9 @@ from mohand.version import get_cli_version
 部分终端默认值会在终端命令定义时进行再初始化赋值
 """
 env = _AttributeDict({
-    'version': get_cli_version(),
+    'version': OrderedDict({
+        'mohand': get_cli_version(),
+    }),
     'plugin_namespace': 'mohand.plugin.hand',
 
     # 终端再赋值
