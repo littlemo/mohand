a68fd139cf1f2a48424c102769896d81d77c33be
Moore.Huang <moore@moorehy.com>
2019-10-04 12:23:22 +0800

    Feature: 在导包时将 vendor 路径加入到 sys.path 中

--- source/mohand/__init__.py
+++ source/mohand/__init__.py
@@ -1,4 +1,10 @@
+import os
+import sys
 from .version import get_cli_version
 
+MOHAND_ROOT = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+MOHAND_VENDOR = os.sep.join([MOHAND_ROOT, "vendor"])
+# Inject vendored directory into system path.
+sys.path.insert(0, MOHAND_VENDOR)
 
 __version__ = get_cli_version()
